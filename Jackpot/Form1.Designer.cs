e7d9edf2d74508947575b88d4ef46333cca318be
sebraymont <sebzilla@live.co.uk>
2015-10-13 14:51:53 +0100

    cod

---
+++ Jackpot/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Jackpot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtCash = new System.Windows.Forms.TextBox();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.lbl_Cash = new System.Windows.Forms.Label();
+            this.lblbets = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(88, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Bet!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(79, 78);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtCash
+            // 
+            this.txtCash.Location = new System.Drawing.Point(79, 169);
+            this.txtCash.Name = "txtCash";
+            this.txtCash.Size = new System.Drawing.Size(100, 20);
+            this.txtCash.TabIndex = 2;
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Location = new System.Drawing.Point(112, 59);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(35, 13);
+            this.lbl_Name.TabIndex = 3;
+            this.lbl_Name.Text = "Name";
+            // 
+            // lbl_Cash
+            // 
+            this.lbl_Cash.AutoSize = true;
+            this.lbl_Cash.Location = new System.Drawing.Point(112, 153);
+            this.lbl_Cash.Name = "lbl_Cash";
+            this.lbl_Cash.Size = new System.Drawing.Size(31, 13);
+            this.lbl_Cash.TabIndex = 4;
+            this.lbl_Cash.Text = "Cash";
+            // 
+            // lblbets
+            // 
+            this.lblbets.AutoSize = true;
+            this.lblbets.Location = new System.Drawing.Point(329, 22);
+            this.lblbets.Name = "lblbets";
+            this.lblbets.Size = new System.Drawing.Size(31, 13);
+            this.lblbets.TabIndex = 5;
+            this.lblbets.Text = "Bets:";
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 361);
+            this.Controls.Add(this.lblbets);
+            this.Controls.Add(this.lbl_Cash);
+            this.Controls.Add(this.lbl_Name);
+            this.Controls.Add(this.txtCash);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtCash;
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.Label lbl_Cash;
+        private System.Windows.Forms.Label lblbets;
+    }
+}
+
